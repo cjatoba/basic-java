f361a4c642fa595c6509f6445990eda89e4442aa
cjatoba <claytonjatoba@gmail.com>
2023-02-19 22:24:13 -0300

    Indentation

---
+++ java-anatomy-classes/src/StudentReportCard.java
@@ -0,0 +1,11 @@
+public class StudentReportCard {
+    public static void main(String[] args) {
+        int finalmedia = 7;
+        if (finalmedia < 6)
+            System.out.println("Disapproved");
+        else if (finalmedia == 6)
+            System.out.println("Mineral proof");
+        else
+            System.out.println("Approved");
+    }
+}
