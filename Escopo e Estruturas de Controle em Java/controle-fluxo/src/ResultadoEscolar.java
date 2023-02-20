0ea4f04188e79306c5b36b782afd1d2972055dfc
cjatoba <claytonjatoba@gmail.com>
2023-02-20 12:53:17 -0300

    Condição ternária

--- Escopo e Estruturas de Controle em Java/controle-fluxo/src/ResultadoEscolar.java
+++ Escopo e Estruturas de Controle em Java/controle-fluxo/src/ResultadoEscolar.java
@@ -1,13 +1,8 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+        int nota = 4;
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
 
-        if (nota >= 7) {
-            System.out.println("Aprovado");
-        } else if (nota >= 5 && nota < 7) {
-            System.out.println("Recuperação");
-        } else {
-            System.out.println("Reprovado");
-        }
+        System.out.println(resultado);
     }
 }
