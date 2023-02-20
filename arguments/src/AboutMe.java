7e556f334cad39b85905fc4d84fb269543bd2b27
cjatoba <claytonjatoba@gmail.com>
2023-02-20 11:09:06 -0300

    Adiciona a classe Scanner

--- arguments/src/AboutMe.java
+++ arguments/src/AboutMe.java
@@ -1,12 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) throws Exception {
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.parseInt(args[2]);
-        double altura = Double.parseDouble(args[3]);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Meu nome é " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos de idade");
-        System.out.println("Minha altura é " + altura + " cm");
+        System.out.println("Digite seu nome: ");
+        String firstName = scanner.next();
+        
+        System.out.println("Digite seu sobrenome: ");
+        String lastName = scanner.next();
+        
+        System.out.println("Digite sua idade: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Digite sua altura (cm): ");
+        double height = scanner.nextDouble();
+
+        System.out.println("Meu nome é " + firstName + " " + lastName);
+        System.out.println("Tenho " + age + " anos de idade");
+        System.out.println("Minha altura é " + height + " cm");
     }
 }
