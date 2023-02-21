8611e8c95b3d8c2c234f171bc2d9c8bf97fb6ad0
cjatoba <claytonjatoba@gmail.com>
2023-02-21 08:26:01 -0300

    Selecionando candidatos

--- Escopo e Estruturas de Controle em Java/controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ Escopo e Estruturas de Controle em Java/controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,30 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2000.0);
-        analisarCandidato(2200.0);
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos() {
+        String[] candidatos = {"MARIA", "JOSE", "FELIPE", "MARIO", "JOSEFA", "LINDADURA"};
+
+        int canditatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (canditatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou R$" + salarioPretendido + " de salário ");
+
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para vaga");
+                canditatosSelecionados++;
+            }
+        }
     }
 
     static void analisarCandidato(double salarioPretendido) {
@@ -17,5 +37,9 @@ public class ProcessoSeletivo {
         } else {
             System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
         }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 }
